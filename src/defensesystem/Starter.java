991d154eecc6fb0b8555609ea102f6cbfd7c3211
mhmharshani <mhmharshani@gmail.com>
2025-11-14 11:41:52 +0530

    initial commit

---
+++ src/defensesystem/Starter.java
@@ -0,0 +1,13 @@
+
+package defensesystem;
+
+import defensesystem.util.Strength;
+
+public class Starter {
+    
+    public static void main(String[] args) {
+        Strength s = Strength.LOW;
+        System.out.println(s);
+    }
+    
+}
